694d08f643ff86cfbea69353b2e1bf277ab01196
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-07-27 14:37:54 +0530

    Create a connection and a MySQL Database name 'newdatasample'

--- CsvReader.py
+++ CsvReader.py
@@ -14,3 +14,17 @@ MySQL Workbench with mysql
 '''
 
 
+# First Run Below Code then comment this and run next code
+# Create A database in Mysql ------>
+
+import mysql.connector as msql
+from mysql.connector import Error
+try:
+    conn = msql.connect(host='localhost', user='root',  
+                        password='j@hirMj123')#give ur username, password
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE NEWDATASAMPLE")
+        print("Database is created")
+except Error as e:
+    print("Error while connecting to MySQL", e)
