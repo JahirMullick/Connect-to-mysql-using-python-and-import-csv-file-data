1b18002a6dcacc7fc34a91b02dc8a16edec389c8
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-07-27 14:35:41 +0530

    Our task

---
+++ CsvReader.py
@@ -0,0 +1,16 @@
+'''
+Connect to mysql using python 
+and import the csv file into mysql 
+and create a table.
+
+
+System requirements :
+Install the pydrive python module as follows :
+pip install mysql-connector-python
+pip install pandas
+
+Install MySQL : 
+MySQL Workbench with mysql
+'''
+
+
